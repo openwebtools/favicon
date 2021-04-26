d2d8ed3b24e75de1d1b39091dd96083a72c630e8
Vivek Sasidharan <mail@viveks.dev>
2021-04-26 22:23:07 +0530

    ico partial gen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
 import { Box, Button, Flex, Heading, Stack } from "@chakra-ui/react";
-import React from "react";
+import React, { RefObject, useState } from "react";
 import Layout from "../src/components/Layout";
+import IcoGenerator from "../src/services/IcoGenerator";
 
 export default function Home() {
+  const [image, setImage] = useState({ preview: "", raw: "" });
+  const icoGenerator = new IcoGenerator();
+  const inputOpenFileRef = React.createRef<any>();
+  const handleChange = async (e) => {
+    if (e.target.files.length) {
+      const file = e.target.files[0] as File;
+      const blob = new Blob([file], { type: file.type });
+      const icoFile = icoGenerator.createIcoFile(blob, 50);
+
+      await icoFile.arrayBuffer().then((x) => console.log(x));
+      setImage({
+        preview: URL.createObjectURL(icoFile),
+        raw: e.target.files[0],
+      });
+    }
+  };
+
+  const handleClick = (e) => {
+    inputOpenFileRef.current.click();
+  };
+
   return (
     <Layout
       title="Favicon Generator | Open Web Tools"
@@ -20,7 +42,7 @@ export default function Home() {
           mt={[8, 10, 12, 24]}
           w={["80%", "80%", "60%", "40%"]}
         >
-          <Button variant="solid" colorScheme="light">
+          <Button variant="solid" colorScheme="dark" onClick={handleClick}>
             Image
           </Button>
           <Button variant="solid" colorScheme="dark">
@@ -29,6 +51,21 @@ export default function Home() {
           <Button variant="solid" colorScheme="dark">
             Emoji
           </Button>
+          <input
+            type="file"
+            ref={inputOpenFileRef}
+            style={{ display: "none" }}
+            onChange={handleChange}
+          />
+
+          {image.preview ? (
+            <>
+              <img src={image.preview} alt="dummy" width="256" height="256" />
+              <a href={image.preview} target="_blank" rel="noopener noreferrer">
+                Download
+              </a>
+            </>
+          ) : null}
         </Stack>
       </Flex>
     </Layout>
--- src/services/IcoGenerator.ts
+++ src/services/IcoGenerator.ts
@@ -1,1 +1,30 @@
-export default class IcoGenerator {}
+export default class IcoGenerator {
+  public createIcoFile(blob: Blob, imageSize: number): Blob {
+    const icoFile = [];
+
+    // Set header
+    icoFile.push(this.createHeader());
+
+    // Set Icon Metadata
+    icoFile.push(new Uint8Array([imageSize <= 256 ? imageSize : 0]).buffer);
+    icoFile.push(new Uint8Array([imageSize <= 256 ? imageSize : 0]).buffer);
+
+    icoFile.push(new Uint8Array([0, 0, 1, 0]).buffer);
+
+    icoFile.push(new Uint8Array([32, 0]).buffer);
+
+    icoFile.push(new Uint32Array([blob.size]).buffer);
+
+    icoFile.push(new Uint32Array([22]).buffer);
+
+    icoFile.push(blob);
+
+    return new Blob(icoFile, {
+      type: "image/vnd.microsoft.icon",
+    });
+  }
+
+  private createHeader(): Uint8Array {
+    return new Uint8Array([0, 0, 1, 0, 1, 0]);
+  }
+}
